17454ed462bdb655896c35b29fa0133d7e65d088
Hazem Bakr <58142945+hazembakr@users.noreply.github.com>
2025-08-25 17:54:22 +0300

    Add validation for user on model level and database level

---
+++ db/migrate/20250825144156_add_email_constraints_to_users.rb
@@ -0,0 +1,7 @@
+class AddEmailConstraintsToUsers < ActiveRecord::Migration[8.0]
+  def change
+    change_column_null :users, :email, false
+
+    add_index :users, :email, unique: true 
+  end
+end
