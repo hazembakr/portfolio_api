284eb7068d5fddc544a857ad52c4903b553e7341
Hazem Bakr <58142945+hazembakr@users.noreply.github.com>
2025-08-25 16:08:56 +0300

    Make the params require and permit instead of expect for more safty

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -41,11 +41,11 @@ class UsersController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
-      @user = User.find(params.expect(:id))
+      @user = User.find(params[:id])
     end
 
     # Only allow a list of trusted parameters through.
     def user_params
-      params.expect(user: [ :name, :email, :address, :image, :password_digest ])
+      params.require(:user).permit(:name, :email, :address, :image, :password_digest)
     end
 end
